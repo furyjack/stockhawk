78ded3cdd746384ca27fb8e57e0e385aa5f3a359
furyjack <lakshaytaneja26@gmail.com>
2016-12-08 20:28:46 +0530

    widget working

--- app/src/main/java/com/udacity/stockhawk/ui/stockAppWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/ui/stockAppWidgetProvider.java
@@ -1,36 +1,76 @@
 package com.udacity.stockhawk.ui;
 
 
+import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.widget.RemoteViews;
 
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.sync.QuoteSyncJob;
+import com.udacity.stockhawk.widgetintentservice;
 
 public class stockAppWidgetProvider extends AppWidgetProvider{
 
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        final int N = appWidgetIds.length;
 
-        // Perform this loop procedure for each App Widget that belongs to this provider
-        for (int i=0; i<N; i++) {
-            int appWidgetId = appWidgetIds[i];
+        for (int appWidgetId:appWidgetIds) {
+
 
             // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            Intent j = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, j, 0);
 
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main_appwidget);
-            views.setOnClickPendingIntent(R.id.recycler_vieww, pendingIntent);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                setRemoteAdapter(context, views);
+            } else {
+                setRemoteAdapterV11(context, views);
+            }
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
+
+
+
     }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if(QuoteSyncJob.ACTION_DATA_UPDATED.equals(intent.getAction()))
+        {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.lvquotes);
+
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.lvquotes,
+                new Intent(context, widgetintentservice.class));
+    }
+
+
+    @SuppressWarnings("deprecation")
+    private void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(0, R.id.lvquotes,
+                new Intent(context,  widgetintentservice.class));
+    }
+
+
 
 }
---
+++ app/src/main/java/com/udacity/stockhawk/widgetintentservice.java
@@ -0,0 +1,128 @@
+package com.udacity.stockhawk;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Binder;
+import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.udacity.stockhawk.data.Contract;
+
+import static com.udacity.stockhawk.data.Contract.Quote.COLUMN_PERCENTAGE_CHANGE;
+import static com.udacity.stockhawk.data.Contract.Quote.COLUMN_PRICE;
+import static com.udacity.stockhawk.data.Contract.Quote.COLUMN_SYMBOL;
+
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class widgetintentservice extends RemoteViewsService {
+
+    public static final String[] QUOTE_COLUMNS = {
+            Contract.Quote._ID,
+            COLUMN_SYMBOL,
+            COLUMN_PRICE,
+            COLUMN_PERCENTAGE_CHANGE,
+
+    };
+
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsFactory() {
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+                // Nothing to do
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+                // This method is called by the app hosting the widget (e.g., the launcher)
+                // However, our ContentProvider is not exported so it doesn't have access to the
+                // data. Therefore we need to clear (and finally restore) the calling identity so
+                // that calls use our process and permission
+                final long identityToken = Binder.clearCallingIdentity();
+                data = getContentResolver().query(Contract.Quote.uri,
+                        QUOTE_COLUMNS,
+                        null,
+                        null,
+                        null);
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+                RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.list_item_quote);
+                String symbol = data.getString(data.getColumnIndex(Contract.Quote.COLUMN_SYMBOL));
+                String price = data.getString(data.getColumnIndex(Contract.Quote.COLUMN_PRICE));
+                String perchange = data.getString(data.getColumnIndex(Contract.Quote.COLUMN_PERCENTAGE_CHANGE));
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                    setRemoteContentDescription(views, symbol);
+                }
+                views.setTextViewText(R.id.symbol, symbol);
+                views.setTextViewText(R.id.price, price);
+                views.setTextViewText(R.id.change, perchange);
+
+
+
+                return views;
+            }
+
+            @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+            private void setRemoteContentDescription(RemoteViews views, String description) {
+                views.setContentDescription(R.id.symbol, description);
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.list_item_quote);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (data.moveToPosition(position))
+                    return data.getLong(data.getColumnIndex(Contract.Quote._ID));
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+
+
+
+}
